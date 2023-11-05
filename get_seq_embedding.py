fe5ea29c64d34cc92710d6b00f875e414688f12b
Thickstem <mosohaso3560@gmail.com>
2023-11-05 12:00:42 +0900

    complete get emb

--- get_seq_embedding.py
+++ get_seq_embedding.py
@@ -1,13 +1,18 @@
+"""Getting sequence embedding code"""
 import argparse
-import numpy as np
-import pandas as pd
+import pickle
+
 import torch
 import fm
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
 
 
 def _argparse():
     args = argparse.ArgumentParser()
-    args.add_argument("--file_path", type=str, help="path to input file")
+    args.add_argument("--i", type=str, help="path to input file")
+    args.add_argument("--o", type=str, help="path to output file")
     args.add_argument(
         "--over_length",
         type=str,
@@ -25,6 +30,8 @@ def _argparse():
 
 
 class GetEmbedding:
+    """Get Embedding class"""
+
     def __init__(self, opt: argparse.Namespace):
         self.max_seq_len = 1022  #
 
@@ -34,10 +41,10 @@ class GetEmbedding:
         )
         self.batch_converter = self.alphabet.get_batch_converter()
         self.model.eval()
-    
-    def _calc_embedding(self,seq:str,seq_name:str)->torch.Tensor:
+
+    def _calc_embedding(self, seq: str, seq_name: str) -> torch.Tensor:
         data = [(f"{seq_name}", f"{seq}")]
-        batch_labels, batch_strs, batch_tokens = self.batch_converter(data)
+        _, _, batch_tokens = self.batch_converter(data)
         with torch.no_grad():
             results = self.model(batch_tokens, repr_layers=[12])
         token_embeddings = results["representations"][
@@ -45,22 +52,56 @@ class GetEmbedding:
         ]  # dim=(1,seq_len+2,emb_dim=640)
         return token_embeddings[0][0]  # return embedding of [CLS] token.
 
-    def get(self, seq: str, seq_name="RNA1")->torch.Tensor:
-        seq_len=len(seq)
-        if seq_len>self.max_seq_len:
-            if self.over_length=="trancate":
-                seq=seq[:self.max_seq_len]   
-                embedding=self._calc_embedding(seq,seq_name)
+    def get(self, seq: str, seq_name="RNA1") -> torch.Tensor:
+        """getting embedding for each sequence
+
+        Args:
+            seq (str): sequence string
+            seq_name (str, optional): seqence name if needed. Defaults to "RNA1".
+
+        Raises:
+            NotImplementedError: When over_length process is not ["trancate","average]
+        Returns:
+            torch.Tensor: embedding tensor
+        """
+        seq_len = len(seq)
+        if seq_len > self.max_seq_len:
+            if self.over_length == "trancate":
+                seq = seq[: self.max_seq_len]
+                embedding = self._calc_embedding(seq, seq_name)
                 return embedding
-            elif self.over_length=="average":
-                seq_fragments=[seq[i:i+self.max_seq_len] for i in range(0, len(seq), self.max_seq_len)]
-                frag_embs=[self._calc_embedding(seq_frag,seq_name) for seq_frag in seq_fragments ] # list[torch.Tensor]
-                frag_embs=torch.tensor(frag_embs)
-                ave_embedding=torch.mean(frag_embs,1) # calc mean along with dim=1
+            elif self.over_length == "average":
+                seq_fragments = [
+                    seq[i : i + self.max_seq_len]
+                    for i in range(0, len(seq), self.max_seq_len)
+                ]
+                frag_embs = [
+                    self._calc_embedding(seq_frag, seq_name)
+                    for seq_frag in seq_fragments
+                ]  # list[torch.Tensor]
+                frag_embs = torch.stack(frag_embs)
+                ave_embedding = torch.mean(frag_embs, 0)  # calc mean along with dim=1
                 return ave_embedding
+            else:
+                raise NotImplementedError()
+
+        else:
+            return self._calc_embedding(seq, seq_name)
+
 
 def main(opt: argparse.Namespace):
-    pass
+    """main"""
+    seq_df = pd.read_csv(opt.i, index_col=0).iloc[:10]
+    embedder = GetEmbedding(opt)
+    emb_array = []
+    seq5utr, seq3utr = seq_df["5UTR"].values, seq_df["3UTR"].values
+
+    for utr5, utr3 in tqdm(zip(seq5utr, seq3utr)):
+        emb5, emb3 = embedder.get(utr5), embedder.get(utr3)
+        emb_array.append([emb5, emb3])
+
+    with open(opt.o, "wb") as f:
+        pickle.dump(emb_array, f)
 
 
 if __name__ == "__main__":
