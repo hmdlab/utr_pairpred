7346747420580dcb88506cba6fcba0c450baee94
Thickstem <mosohaso3560@gmail.com>
2025-03-24 12:48:31 +0900

    add sh

--- scripts/get_feature.sh
+++ scripts/get_feature.sh
@@ -1,6 +1,4 @@
 python ../preprocess/get_seq_embedding.py \
-		--i ../data/human/gencode44_utr_gene_unique_cdhit09_max10k.csv \
+		--i ../data/human/gencode44_utr_gene_unique_cdhit09.csv \
 		--o ../data/human/\
 		--feature_craft 
-
-# --output should be dir in this pattern 
