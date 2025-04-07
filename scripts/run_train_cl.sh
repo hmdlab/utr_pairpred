f4cec03a10f08b2fcc42e59777cc170b5aa4cfc6
Thickstem <s.thickstem@gmail.com>
2025-04-07 13:02:12 +0900

    update readme

--- scripts/run_train_cl.sh
+++
@@ -1,2 +0,0 @@
-poetry run python ../src/run_train_cl.py \
-	--cfg ../config/human/contrastive_learning_10foldval_rinalmo.yaml \
--- scripts/run_train_sv.sh
+++
@@ -1,2 +0,0 @@
-poetry run python ../src/run_train_sv.py \
-	--cfg ../config/human/mlp_split_large_rinalmo_10fold.yaml \
